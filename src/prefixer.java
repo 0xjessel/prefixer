8bd955a34c222088284b31377634a052ca307d77
Jesse Chen <Jesse Chen@JesseChen-PC>
2011-09-28 22:11:40 -0700

    added opening and closing parentheses to the prefix expression

--- src/prefixer.java
+++ src/prefixer.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -12,7 +11,7 @@ public class prefixer {
 
 		// command line argument check
 		if (args.length != 1) {
-			System.err.println("exactly one argument required");
+			System.err.println("exactly one argument required (the filename)");
 			System.exit(1);
 		}
 
@@ -24,8 +23,13 @@ public class prefixer {
 		System.out.println(prefixString);
 	}
 
+	/**
+	 * algorithm to convert an infix expression to a prefix version
+	 * 
+	 * the trick is to reverse the input string and use two stacks, one stack
+	 * for the output and one for holding operators.
+	 */
 	private static String infixToPrefix(String expression) {
-		StringBuffer toReturn = new StringBuffer();
 		StringBuffer reversed = new StringBuffer(expression).reverse();
 		StringTokenizer tokens = new StringTokenizer(reversed.toString());
 		Stack<String> stack = new Stack<String>();
@@ -56,6 +60,7 @@ public class prefixer {
 					}
 				} while (done == false);
 			} else if (token.equals("(")) {
+				// pop operators from stack until a ")" is encountered
 				while (!stack.empty()) {
 					String operator = stack.pop();
 					if (!operator.equals(")")) {
@@ -67,20 +72,73 @@ public class prefixer {
 			}
 		}
 
+		// push remaining operators into output
 		while (!stack.empty()) {
 			output.push(stack.pop());
 		}
-		
+
+		return buildOutputString(output);
+	}
+
+	/**
+	 * constructs the prefix expression
+	 * 
+	 * reverses the output Stack from infixToPrefix, meanwhile adding "(" and
+	 * ")" when needed
+	 */
+	private static String buildOutputString(Stack<String> output) {
+		StringBuffer toReturn = new StringBuffer();
+
+		// to mark when we found an operator so we can prepend a "("
+		boolean foundOperator = false;
+		// used in conjunction with foundOperator to count two operands before
+		// appending a ")"
+		int operatorCount = 0;
+		// used to determine if we need a ")" at the end of the entire
+		// expression
+		int leftParenCount = 0;
+		int rightParenCount = 0;
+
 		while (!output.empty()) {
-			toReturn.append(output.pop());
-			toReturn.append(" ");
+			String op = output.pop();
+
+			// if the next output is an operator, prepend a "("
+			if (isOperator(op)) {
+				toReturn.append("(");
+				// if a previous operand was already found, but operatorCount
+				// hasn't reached 2, then reset the counter for the ")"
+				if (foundOperator) {
+					operatorCount = 0;
+				} else {
+					foundOperator = true;
+				}
+				leftParenCount++;
+			} else if (foundOperator) {
+				operatorCount++;
+			}
+
+			toReturn.append(op);
+
+			// once we hit two consecutive operands, append a ")" to close
+			if (operatorCount == 2) {
+				toReturn.append(")");
+				rightParenCount++;
+				operatorCount = 0;
+			}
+
+			if (!output.empty()) {
+				toReturn.append(" ");
+			} else if (rightParenCount < leftParenCount) {
+				toReturn.append(")");
+			}
 		}
+
 		return toReturn.toString();
 	}
 
-	// true is a is lower pri than b, false if a is higher pri than b
+	// TRUE if a is lower priority than b, FALSE if a is higher priority than b
 	private static boolean isLowerPri(String a, String b) {
-		return "+*-/".indexOf(a.charAt(0))%2 < "+*-/".indexOf(b.charAt(0))%2;
+		return "+*-/".indexOf(a.charAt(0)) % 2 < "+*-/".indexOf(b.charAt(0)) % 2;
 	}
 
 	private static boolean isOperand(String token) {
